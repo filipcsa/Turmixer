94ec6e8ef81c64e7b9a4904eff9f6ea4afde4f7a
filipcsa <filip.csaba@email.cz>
2019-06-02 11:09:42 +0200

    idb basics

---
+++ idb.js
@@ -0,0 +1,108 @@
+// Based on the MDN example
+// https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
+// vendor prefixes hahaaa, dalsi body
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || {READ_WRITE: "readwrite"};
+window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+
+if (!window.indexedDB) {
+    window.alert("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
+}
+
+const DB_NAME = 'playlistDB0003';
+const DB_VERSION = 3;
+const DB_STORE_NAME = 'playlists';
+
+var db;
+
+function openDb() {
+    var req = indexedDB.open(DB_NAME, DB_VERSION);
+    req.onsuccess = function (evt) {
+        db = this.result;
+        console.log("openDb DONE");
+        // test getPlaylistSongAndDisplayInCustomPlaylist();
+    };
+    req.onerror = function (evt) {
+        console.log("F*CK THIS SH*T IM OUT");
+    };
+    req.onupgradeneeded = function (evt) {
+        console.log("openDb.onupgradeneeded");
+        var store = evt.currentTarget.result.createObjectStore(
+          DB_STORE_NAME);
+  
+        //store.createIndex('title', 'title', { unique: true });
+    };
+}
+
+function getObjectStore(store_name, mode) {
+    var tx = db.transaction(store_name, mode);
+    return tx.objectStore(store_name);
+}
+
+function clearObjectStore() {
+    var store = getObjectStore(DB_STORE_NAME, 'readwrite');
+    var req = store.clear();
+    req.onsuccess = function(evt) {
+      console.log("ObjectStore cleared!")
+    };
+    req.onerror = function (evt) {
+      console.error("clearObjectStore:", evt.target.errorCode);
+      displayActionFailure(this.error);
+    };
+}
+
+// Saves a playlist to IDB
+// The playlist has(?) to be chanded to simple object
+// {title, list of audio files}
+function addPlaylist(playlist) {
+    let audioFiles = playlist.tracks.map(t => t.file);
+    let plObj = {title: playlist.title, files: audioFiles};
+    var store = getObjectStore(DB_STORE_NAME, 'readwrite');
+    var req;
+    try{
+        req = store.put(plObj, playlist.title);
+    } catch (e) {
+        if (e.name == 'DataCloneError')
+            console.log("PROBLEM WITH CHROME");
+        throw e;
+    }
+    req.onsuccess = function (evt) {
+        console.log("Playlist added to db");
+    }
+    req.onerror = function() {
+        console.log("Task of adding playlist to db failed successfuly");
+    }
+}
+
+// test function to see if it can handle audio blobs
+function addPlaylistSong(blob) {
+    var store = getObjectStore(DB_STORE_NAME, 'readwrite');
+    var req;
+    try{
+        req = store.put(blob, "song");
+    } catch (e) {
+        if (e.name == 'DataCloneError')
+            console.log("PROBLEM WITH CHROME");
+        throw e;
+    }
+    req.onsuccess = function (evt) {
+        console.log("SONG ADDED TO DB");
+    }
+    req.onerror = function() {
+        console.log("F*CK THE DB!! I HATE MY LIFE");
+    }
+}
+
+// test function 2
+function getPlaylistSongAndDisplayInCustomPlaylist() {
+    var store = getObjectStore(DB_STORE_NAME, 'readwrite');
+    var req = store.get("song");
+    req.onsuccess = function(evt) {
+        var song = evt.target.result;
+        console.log("Song retrieved: " + song);
+        p2.addSong(song);
+    }
+}
+
+openDb();
+
---
+++ playlist.js
@@ -0,0 +1,164 @@
+ /** Class for representing a song in the playlist */
+ class Song {
+     constructor(src, title, interpret, imgsrc, file) {
+        this.src = src;
+        this.title = title;
+        this.artist = interpret;
+        this.imgsrc = imgsrc;
+
+        // for saving to idb
+        this.file = file
+     }
+
+     getSongDiv() {
+         // create elements for img, title and artist
+         let songDiv = document.createElement('div');
+         songDiv.classList.add('song');
+         let albumImg = document.createElement('img');
+         albumImg.src = this.imgsrc;
+         let title = document.createElement('h6');
+         title.innerText = this.title;
+         let artist = document.createElement('h6');
+         artist.innerText = this.artist;
+
+         // insert the into the div
+         songDiv.appendChild(albumImg);
+         songDiv.appendChild(title);
+         songDiv.appendChild(artist);
+
+         return songDiv;
+     }
+ }
+        
+ /** A class to represent an immutable playlist of songs
+  * this playlist will be programatically defined in turmix.html
+  * On the other hand the mutable playlist can be created by the user */ 
+class Playlist {
+    constructor(title) {
+        this.title = title;
+        this.tracks = [];
+        this.selectedTrack = null;
+        this.type = "immutable";
+
+        // create list item, add event listener and add to DOM
+        this.li = document.createElement('li');
+        this.li.innerText = title;
+        this.li.addEventListener('click', this.onPlaylistClicked.bind(this), false);
+        let playlistsUL = document.querySelector('.playlists');
+        playlistsUL.appendChild(this.li);
+        console.log(playlistsUL);
+    }
+    
+    // when this playlist is clicked
+    onPlaylistClicked() {
+        if (selectedPlaylist != null) {
+            selectedPlaylist.li.classList.remove('selected');
+        }    
+        this.li.classList.add('selected');
+        selectedPlaylist = this;
+        console.log(`${this.title} clicked!`);
+        this.displayPlaylist();
+    }
+
+    // displays the whole playlist
+    displayPlaylist() {
+        console.log("Displaying playlist " + this.title);
+        console.log("Consisting of tracks " + this.tracks);
+        selectedPlaylistDiv.innerHTML = "";
+        this.tracks.forEach(this.displaySong.bind(this));
+    }
+
+    // displays the song and adds event listener to it
+    displaySong(song) {
+        /*
+        let trackView = document.createElement('li');
+        
+        let url =  track;//"https://cors-anywhere.herokuapp.com/" + track;
+        /*
+        jsmediatags.read(url, {
+            onSuccess: function(tag) {
+                console.log(tag);
+                el.innerText = tag.tags.title;
+            }
+        });
+        
+       let blob = fetch(track).then(r => r.blob());
+        getMetadata(blob, trackView);
+        
+        console.log("Dispalying track " + track);
+        //trackView.innerText = tags;
+        trackView.track = track;
+        trackView.addEventListener('click', this.onTrackClicked, false);
+        selectedPlaylistDiv.appendChild(trackView);
+        */
+       let songDiv = song.getSongDiv();
+       console.log(songDiv);
+       songDiv.songSrc = song.src;
+       console.log(songDiv);
+       songDiv.addEventListener('click', this.onSongClicked.bind(null, event, song.src));
+       selectedPlaylistDiv.appendChild(songDiv);
+       
+
+    }
+
+    // plays the song, has to toggle the play button
+    onSongClicked(event, songSrc) {
+        audio.src = songSrc;
+        audio.play();
+        playToggle.classList = "";
+        playToggle.classList.add('play');
+        console.log(playToggle);
+        console.log("Track clicked");
+    }
+
+    // method to add tracks, can add track song or array of tracks
+    addSong(song) {
+        if (Array.isArray(song)) {
+            this.tracks = this.tracks.concat(song);
+        }
+        else { 
+            this.tracks.push(song);
+        }
+    }
+}
+
+
+class CustomPlaylist extends Playlist {
+    constructor(title) {
+        super(title);
+        this.type = "custom";
+        this.tracks = [];
+    }
+
+    /** Add song by Dn'D, get metadata w/ the getMetadata func */
+    addSong(file) {
+        console.log("Called addSong on CustomPlaylist");
+        this.getMetadata(file, this);
+    }
+
+    /** Loads song from IDB, get metadata w/ loadMetadata func 
+     * works basically the same as addSong, but doesn't display
+     * but doesn't display the playlist immediately
+    */
+    loadSong(file) {
+        
+    }
+
+    /** Adding song to selected playlist manually by Dn'D */
+    getMetadata(data) {
+        let song = new Song('','','','', data);
+        musicmetadata(data, function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            song.title = result.title;
+            song.artist = result.artist;
+            song.src = URL.createObjectURL(data);
+            console.log(result.artist);
+            selectedPlaylist.tracks.push(song);
+            selectedPlaylist.displayPlaylist();
+            // add to idb
+            addPlaylist(selectedPlaylist);
+        });
+    }
+
+}
